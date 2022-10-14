3537f150a3841ea23276be12548679cf469904c2
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-10-14 10:06:19 +0530

    login message popup

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -4,7 +4,7 @@ export default class TodoApp extends Component {
     render() {
         return (
             <div>
-                My Todo Application
+                {/* My Todo Application */}
                 <div>
                     <LoginComponent />
                 </div>
@@ -18,44 +18,79 @@ class LoginComponent extends Component {
         super(props)
         this.state = {
             username: "gaurab",
-            password: ""
+            password: "",
+            hasLoginFailed: false,
+            showSuccessMessage: false
         }
 
-        // this.handleusernameChange = this.handleusernameChange.bind(this);
-        // this.handlePasswordChange = this.handlePasswordChange.bind(this);
         this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
 
     }
+    showInvalidCredentials(props) {
+        if (props.hasLoginFailed) {
+            return <div>Invalid credentials</div>
+        }
+        else {
+            return null
+        }
+    }
 
-    // handleusernameChange(event) {
-    //     console.log(event.target.value);
-    //     this.setState({
-    //         username: event.target.value
-    //     })
-    // }
+    showLoginSuccessMessage(props) {
+        if (props.showSuccessMessage) {
+            return <div>Login Successful</div>
+        }
+        else {
+            return null
+        }
+    }
 
-    // handlePasswordChange(event) {
-    //     console.log(event.target.value);
-    //     this.setState({
-    //         password: event.target.value
-    //     })
-    // }
 
     handleChange(event) {
-        console.log(event.target.name);
+        // console.log(event.target.name);
         this.setState({
             [event.target.name]
                 : event.target.value
         })
     }
 
+    loginClicked() {
+        // console.log(this.state);
+        if (this.state.username === 'gaurab' && this.state.password === 'gaurab123') {
+            // console.log("Login Successful");
+            this.setState({
+                showSuccessMessage: true
+            })
+
+            this.setState({
+                hasLoginFailed: false
+            })
+        }
+        else {
+            // console.log("Login failed");
+            this.setState({
+                showSuccessMessage: false
+            })
+
+            this.setState({
+                hasLoginFailed: true
+            })
+        }
+
+    }
+
+
+
     render() {
         return (
             <div>
+                <this.showInvalidCredentials hasLoginFailed={this.state.hasLoginFailed} />
+                <this.showLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage} />
                 User Name: <input type="text" name='username' value={this.state.username} onChange={this.handleChange} />
                 Password: <input type="password" name='password' value={this.state.password} onChange={this.handleChange} />
-                <button>Login</button>
+                <button onClick={this.loginClicked}>Login</button>
             </div>
         )
     }
 }
+
