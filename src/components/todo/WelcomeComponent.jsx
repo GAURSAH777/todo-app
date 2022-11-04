2cfd34c094c5943fb6fe76eb9e7f2173b988367e
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-11-04 12:44:25 +0530

    customised welcome message

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -2,12 +2,24 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class WelcomeComponent extends Component {
+    constructor(props){
+        super(props)
+        this.retriveWelcome = this.retriveWelcome.bind(this)
+    }
+
+    retriveWelcome(){
+        console.log('retrive')
+    }
     render() {
         return (
             <>
                 <h1>Welcome!</h1>
                 <div className='container'>Welcome Gaurab,
                     Click <Link to={`/todo`}>here </Link> to manage Todos App.
+                </div>
+                <div className='container'>
+                    Click Here to retrive customized welcome message.
+                    <button onClick={this.retriveWelcome} className='btn btn-success'>Message</button>
                 </div>
             </>
         )
